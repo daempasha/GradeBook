e6531d108b1276e2fc162a9a97511a14014798c7
daempasha <daempasha@gmail.com>
2020-06-14 00:21:47 +0100

    Initial Commit - Created basic structure of the program and created some tests

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book 
+    {
+        private string name;
+        private List<double> grades;
+
+        public Book(string name){
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade){
+            grades.Add(grade);
+        }
+
+        public Statistics GetStatistics(){
+            Statistics result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach(double grade in this.grades){
+                result.High = Math.Max(result.High, grade);
+                result.Low = Math.Min(result.Low, grade);
+                result.Average += grade;    
+            }
+            result.Average /= grades.Count;
+            
+            return result;
+        }
+        
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            Statistics stats = book.GetStatistics();
+
+            Console.WriteLine($"Average grade is: {stats.Average:N1} \nHighest grade is: {stats.High} \nMin grade is: {stats.Low}");
+
+        }
+    }
+}
